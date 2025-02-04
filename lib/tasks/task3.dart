330d75b389eb51dc686230ff9480eae4070403f4
mosabfarayeh <msbalfrayh09@ieee.org>
2025-02-04 13:58:35 +0300

    task3and task4 4/2/2025  01:58

---
+++ lib/tasks/task3.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//ListViewExample
+class Task3 extends StatelessWidget {
+  Task3({super.key});
+  final products = List.generate(100, (index) => 'Product $index');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Example'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) => ListTile(
+          tileColor: (index % 2 == 0) ? Colors.grey[200] : Colors.white,
+          title: Text('Product $index'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tasks/task4.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+//SliverAppBarExample
+class Task4 extends StatelessWidget {
+  const Task4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          // part 1
+          SliverAppBar(
+            pinned: true,
+            floating: false,
+            expandedHeight: 300,
+            //backgroundColor: Colors.red,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('SliverAppBar Example',
+                  style: TextStyle(color: Colors.black)),
+              background: Image.network(
+                'https://media.istockphoto.com/id/1412131208/vector/abstract-orange-and-red-gradient-geometric-shape-circle-background-modern-futuristic.jpg?s=612x612&w=0&k=20&c=V_It1LyqTdBTOvCY8-CBOOj4bh4sFOq8im9gTlHUfPo=',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // part 2
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (_, index) {
+                return ListTile(
+                  title: Text('youtube video  $index'),
+                );
+              },
+              childCount: 50,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

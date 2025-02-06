c0615f4047f4fc9e8cba16f76279c1806caeb063
mosabfarayeh <msbalfrayh09@ieee.org>
2025-02-06 12:15:37 +0300

    add task5 and edit the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'tasks/task1.dart';
+import 'tasks/task5.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Task1(),
+      home: const Task5(),
     );
   }
 }

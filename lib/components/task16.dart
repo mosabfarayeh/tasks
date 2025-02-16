85a8dd9140c4735a07cbcfd92ca40edc038f130f
mosabfarayeh <msbalfrayh09@ieee.org>
2025-02-16 14:33:50 +0300

    update task16

--- lib/components/task16.dart
+++ lib/components/task16.dart
@@ -1,37 +1,66 @@
+// task 16 reusable compo
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed;
-  CustomButton({required this.text, required this.onPressed});
+class Task16 extends StatelessWidget {
+  const Task16({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reusable components"),
       ),
-      child: Text(text, style: TextStyle(fontSize: 16)),
+      body: Center(
+        child: Column(
+          children: [
+            // 1
+            Row(
+              children: [
+                CustomButton(
+                  text: 'Button1',
+                  onPressed: () {
+                    print("Button1 Pressed");
+                  },
+                ),
+                CustomButton(
+                  text: 'Button2',
+                  onPressed: () {
+                    print("Button2 Pressed");
+                  },
+                ),
+              ],
+            ),
+            // 2
+            ElevatedButton(
+              onPressed: () {
+                print("Button2 Pressed");
+              },
+              child: Text("Button2"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyApp extends StatelessWidget {
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Reusable␣Widgets␣Example")),
-        body: Center(
-          child: CustomButton(
-            text: "Click␣Me",
-            onPressed: () {
-              print("Button␣Pressed");
-            },
-          ),
-        ),
-      ),
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
+      onPressed: onPressed,
+      child: Text(text),
     );
   }
 }
